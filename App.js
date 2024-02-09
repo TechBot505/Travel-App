d34715d420ea69802325874c34526fc173c860e5
Rohit <rohitdhanotia2003@gmail.com>
2024-02-09 18:55:14 +0530

    project setup

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard, OnBoardingScreen, PersonalList, Profile, SharedList, LoginScreen } from './app/screens';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+  const screenOptions = {
+    tabBarShowLabel: false,
+    headerShown: false,
+    tabBarStyle: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
+      elevation: 0,
+      backgroundColor: "#ffffff",
+      borderRadius: 15,
+      height: 60,
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={screenOptions}>
+        <Tab.Screen 
+          name="Dashboard"  
+          component={Dashboard} 
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={{alignItems: "center", justifyContent: "center"}}>
+                <Entypo name="home" size={24} color="black" />
+                <Text style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>Dashboard</Text>
+              </View>
+            )
+          }} 
+        />
+        <Tab.Screen 
+          name="PersonalList" 
+          component={PersonalList} 
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={{alignItems: "center", justifyContent: "center"}}>
+                <Entypo name="list" size={24} color="black" />
+                <Text style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>Personal List</Text>
+              </View>
+            )
+          }}
+        />
+        <Tab.Screen 
+          name="SharedList" 
+          component={SharedList} 
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={{alignItems: "center", justifyContent: "center"}}>
+                <Entypo name="share" size={24} color="black" />
+                <Text style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>Shared List</Text>
+              </View>
+            )
+          }}
+        />
+        <Tab.Screen 
+          name="Profile" 
+          component={Profile}
+          options={{
+            tabBarIcon: ({focused}) => (
+              <View style={{alignItems: "center", justifyContent: "center"}}>
+                <FontAwesome5 name="user-tie" size={24} color="black" />
+                <Text style={{color: focused ? "#e32f45" : "#748c94", fontSize: 12}}>Profile</Text>
+              </View>
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ app/screens/index.js
@@ -0,0 +1,8 @@
+import Dashboard from "./Dashboard";
+import Profile from "./Profile";
+import OnBoardingScreen from "./OnBoardingScreen";
+import PersonalList from "./PersonalList";
+import SharedList from "./SharedList";
+import LoginScreen from "./LoginScreen";
+
+export { Dashboard, Profile, OnBoardingScreen, PersonalList, SharedList, LoginScreen };
